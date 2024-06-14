a0d7f1ba908d1bbbee8415729aebeb236240d497
akashwani2209 <akashwani2209@gmail.com>
2024-06-14 21:33:47 +0530

    First Commit

---
+++ paymentsLogin/src/main/java/com/example/paymentsLogin/PaymentsLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentsLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsLoginApplication.class, args);
+	}
+
+}
---
+++ paymentsLogin/src/test/java/com/example/paymentsLogin/PaymentsLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentsLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
