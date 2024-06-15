f7c1d72690e03104fe1b6749f238a2e53c5f3f69
akashwani2209 <akashwani2209@gmail.com>
2024-06-15 22:52:27 +0530

    controller for registration made

---
+++ paymentsLogin/src/main/java/com/example/paymentsLogin/Controller/RegistrationController.java
@@ -0,0 +1,34 @@
+package com.example.paymentsLogin.Controller;
+
+import com.example.paymentsLogin.Model.User;
+import com.example.paymentsLogin.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute @Valid User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "register";
+        }
+        userService.registerUser(user);
+        return "redirect:/login";
+    }
+}
