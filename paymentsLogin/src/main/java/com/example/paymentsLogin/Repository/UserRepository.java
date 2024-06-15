24887703f79d3cd94d3625428be61ebad995f0bb
akashwani2209 <akashwani2209@gmail.com>
2024-06-15 18:27:14 +0530

    MongodbRepository interface extending for search operations

---
+++ paymentsLogin/src/main/java/com/example/paymentsLogin/Repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.paymentsLogin.Repository;
+
+import com.example.paymentsLogin.Model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByUsername(String username);
+}
