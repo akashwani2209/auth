7e88383ef6b3a01b1074e7a908da67fb85348f38
akashwani2209 <akashwani2209@gmail.com>
2024-06-15 20:41:13 +0530

    added registration functionality.

---
+++ paymentsLogin/src/main/java/com/example/paymentsLogin/Service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.paymentsLogin.Service;
+
+import com.example.paymentsLogin.Model.User;
+import com.example.paymentsLogin.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
